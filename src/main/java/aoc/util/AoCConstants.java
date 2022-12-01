dfce577aba223dc843b874abac89497b09946e22
anth <anth@netcompany.com>
2022-12-01 16:29:04 +0100

    Create AoCConstants.java

---
+++ src/main/java/aoc/util/AoCConstants.java
@@ -0,0 +1,11 @@
+package aoc.util;
+
+public interface AoCConstants {
+    String SESSION_ID = "???";
+    String SESSION_ID_STRING = String.format("session=%s", SESSION_ID);
+
+    String INPUT_URL = "https://adventofcode.com/2022/day/%d/input";
+    String INPUT_FILE_NAME = "input.txt";
+    String INPUT_FILE_FOLDER = "src/main/resources/day%02d";
+
+}
